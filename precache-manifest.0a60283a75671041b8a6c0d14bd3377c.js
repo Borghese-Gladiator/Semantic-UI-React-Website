68e8a9e24bd67c29c21d167164d673f941bac992
Timothy Shee <timothy_shee@hotmail.com>
2020-09-01 21:11:46 -0400

    Updates

---
+++ precache-manifest.0a60283a75671041b8a6c0d14bd3377c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3265605a62e2b8317df163c124cae2",
+    "url": "/Semantic-UI-React-Website/index.html"
+  },
+  {
+    "revision": "1ad0b0427d26a73d4551",
+    "url": "/Semantic-UI-React-Website/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "e3731f1fba5b735d0045",
+    "url": "/Semantic-UI-React-Website/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1ad0b0427d26a73d4551",
+    "url": "/Semantic-UI-React-Website/static/js/2.f94ae178.chunk.js"
+  },
+  {
+    "revision": "09f87f5c96aaacde082c5444ddde62e0",
+    "url": "/Semantic-UI-React-Website/static/js/2.f94ae178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3731f1fba5b735d0045",
+    "url": "/Semantic-UI-React-Website/static/js/main.3c1967a8.chunk.js"
+  },
+  {
+    "revision": "c1c650e7cf62744440be",
+    "url": "/Semantic-UI-React-Website/static/js/runtime-main.77eb8ea9.js"
+  },
+  {
+    "revision": "ac3100070a53fdb1fafc3aca898bf270",
+    "url": "/Semantic-UI-React-Website/static/media/Current_Resume.ac310007.pdf"
+  },
+  {
+    "revision": "bd948c3028be74b2c9a9974a806c2930",
+    "url": "/Semantic-UI-React-Website/static/media/RSA_Security_Logo.bd948c30.png"
+  },
+  {
+    "revision": "ba9f6ee27339ab39638c95eff8fb6130",
+    "url": "/Semantic-UI-React-Website/static/media/community-hero.ba9f6ee2.png"
+  },
+  {
+    "revision": "3942d6ef5b2e0d3732082bd9b28c853f",
+    "url": "/Semantic-UI-React-Website/static/media/intro-bg.3942d6ef.png"
+  },
+  {
+    "revision": "ccbb75272f5aee66be9b8517c1e65ce1",
+    "url": "/Semantic-UI-React-Website/static/media/kitten-puppy-cute.ccbb7527.png"
+  },
+  {
+    "revision": "d2adc0d15869d3a5d9826021c6aaac14",
+    "url": "/Semantic-UI-React-Website/static/media/leap-motion.d2adc0d1.png"
+  },
+  {
+    "revision": "406466fbdcbaf37a06726442d3f983cb",
+    "url": "/Semantic-UI-React-Website/static/media/umass.406466fb.png"
+  }
+]);
